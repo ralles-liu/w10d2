5cacd56700e0dfeaa3adfaef2757eae26ebf5990
Jose Miralles <josemiralles1991@gmail.com>
2021-01-05 15:39:25 -0500

    Tabs working

--- widgets/frontend/tabs.jsx
+++ widgets/frontend/tabs.jsx
@@ -2,5 +2,45 @@ import React from "react";
 import ReactDOM from 'react-dom';
 
 export default class Tabs extends React.Component {
-    
+    constructor(props){
+        super(props);
+
+        this.state = {
+            selected: 1,
+        }
+
+        this.handleTabClick = this.handleTabClick.bind(this);
+    }
+
+    handleTabClick(e){
+        const name = e.currentTarget.getAttribute("name");
+        console.log(name);
+        this.setState(
+            {selected: eval(name)}
+        );
+    }
+
+    render(){
+
+        const mappedTabs = this.props.tabs.map((tab, i) => {
+            if (i === this.state.selected) {
+                return (
+                    <li name={i} key={i} onClick={this.handleTabClick}>
+                        <h1>{tab.title}</h1>
+                        <article>{tab.content}</article>
+                    </li>
+                );
+            } else {
+                return <li name={i} key={i} onClick={this.handleTabClick}><h1>{tab.title}</h1></li>;
+            }
+        });
+
+        return(
+            <>
+                <ul>
+                    {mappedTabs}
+                </ul>
+            </>
+        );
+    }
 }
--- widgets/widgets.jsx
+++ widgets/widgets.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Clock from './frontend/clock';
-import Clock from './frontend/clock';
+import Tabs from "./frontend/tabs";
+
 // import Congrats from './congrats';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<Clock/>, root);
+
+  const tabs = [
+    {title: "First Tab", content: "Does blue horseshoe love Anacot Steel?"},
+    {title: "Second Tab", content: "This is the second tab."},
+    {title: "Third Tab", content: "This is the third tab."}
+  ];
+
+  ReactDOM.render(
+      <>
+        <Clock/>
+        <Tabs tabs={tabs}/>
+      </>
+    , root);
 });
